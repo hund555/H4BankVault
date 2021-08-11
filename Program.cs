d8097286b61ba9c88f3386ef4c1dcf1552216ceb
Allan Holm Iversen <alla1862@elevcampus.dk>
2021-08-11 11:13:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H4BankVault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
